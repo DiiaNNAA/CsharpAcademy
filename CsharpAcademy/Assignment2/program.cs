2e1a89ea790eee6353933514416c31752f454271
DiiaNNAA <diana.khabazi.1@btu.edu.ge>
2023-07-03 04:22:08 +0400

    Add project files.

---
+++ CsharpAcademy/Assignment2/program.cs
@@ -0,0 +1,108 @@
+﻿namespace CsharpAcademy.Assignment2
+{
+
+    //public class Task2
+    //{
+    //    public static void Main()
+    //    {
+    //        Console.WriteLine("Enter the temperature in Celsius: ");
+    //        string input = Console.ReadLine();
+
+    //        if (int.TryParse(input, out int temperature))
+    //        {
+    //            if (temperature < 0)
+    //            {
+    //                Console.WriteLine("Freezing");
+    //            }
+    //            else if (temperature >= 0 && temperature <= 30)
+    //            {
+    //                Console.WriteLine("Good weather");
+    //            }
+    //            else
+    //            {
+    //                Console.WriteLine("It's hot");
+    //            }
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Invalid input. Please enter a valid temperature.");
+    //        }
+    //    }
+    //}
+
+
+
+
+
+    //public class Task3
+    //{
+    //    public static void Main()
+    //    {
+    //        string[] choices = { "rock", "paper", "scissors" };
+    //        var rand = new Random();
+    //        int computerChoiceIndex = rand.Next(0, 3);
+    //        string computerChoice = choices[computerChoiceIndex];
+
+    //        Console.WriteLine("Enter your choice (rock, paper, or scissors): ");
+    //        string userChoice = Console.ReadLine().ToLower();
+
+    //        Console.WriteLine("Your choice: " + userChoice);
+    //        Console.WriteLine("Computer's choice: " + computerChoice);
+
+    //        if (userChoice == computerChoice)
+    //        {
+    //            Console.WriteLine("It's a tie");
+    //        }
+    //        else if (
+    //            (userChoice == "rock" && computerChoice == "scissors") ||
+    //            (userChoice == "paper" && computerChoice == "rock") ||
+    //            (userChoice == "scissors" && computerChoice == "paper")
+    //        )
+    //        {
+    //            Console.WriteLine("You win");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Computer wins");
+    //        }
+    //    }
+    //}
+
+
+
+    //public class Task4
+    //{
+    //    static void Main()
+    //    {
+    //        Console.WriteLine("Enter your year of birth: ");
+    //        string input = Console.ReadLine();
+
+    //        if (int.TryParse(input, out int yearOfBirth))
+    //        {
+    //            int chineseYear = (yearOfBirth - 4) % 12;
+
+    //            Console.WriteLine($"You were born in the year of the {GetChineseZodiac(chineseYear)}.");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Invalid input.");
+    //        }
+    //    }
+
+    //    static string GetChineseZodiac(int year)
+    //    {
+    //        string[] chineseZodiacs = {
+    //        "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
+    //        "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"
+    //    };
+
+    //        return chineseZodiacs[year];
+    //    }
+    //}
+
+
+
+
+
+
+}
---
+++ CsharpAcademy/Assignment5/program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace CsharpAcademy.Assignment5
+{
+    public class Task1
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Enter the length of the array: ");
+            string lengthInput = Console.ReadLine();
+
+            if (int.TryParse(lengthInput, out int length) && length > 0)
+            {
+                int[] numbers = GenerateRandomArray(length);
+                Console.WriteLine("Randomly generated array:");
+                PrintArray(numbers);
+
+                Console.WriteLine("Enter the index of the element: ");
+                string indexInput = Console.ReadLine();
+
+                if (int.TryParse(indexInput, out int index) && index >= 0 && index < length)
+                {
+                    int sum = SumDigits(numbers, index);
+                    Console.WriteLine($"Sum of digits in element at index {index}: {sum}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input or index out of range.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid length.");
+            }
+        }
+
+        static int[] GenerateRandomArray(int length)
+        {
+            var random = new Random();
+            int[] numbers = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] = random.Next();
+            }
+
+            return numbers;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (int number in array)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static int SumDigits(int[] array, int index)
+        {
+            int number = array[index];
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+
+            return sum;
+        }
+
+    }
+}
